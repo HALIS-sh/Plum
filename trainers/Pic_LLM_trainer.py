b60a0dc42d2f0bcce5a6ea74453d8947742176ae
HALIS-sh <412929301@qq.com>
2024-03-02 13:06:06 +0000

    update LLM_trainer.py

--- trainers/Pic_LLM_trainer.py
+++ trainers/Pic_LLM_trainer.py
@@ -4,6 +4,7 @@ import time
 import sys 
 sys.path.append("..") 
 import openai
+import re
 from tenacity import (
     retry,
     stop_after_attempt,
@@ -11,106 +12,43 @@ from tenacity import (
 )  # for exponential backoff
 
 
+
+
 class Pic_LLMMixin(SimpleTrainer):
     
+    def get_api_key(self):
+        # api-key file-path
+        file_path = '/home/wenhesun/Plum_Final/api-key.txt'
+        output_file_path = '/home/wenhesun/Plum_Final/get_api-key.txt'
+        # list which is used to restored the result
+        matched_strings = []
+
+        # Read the file by line
+        with open(file_path, 'r', encoding='utf-8') as file:
+            for line in file:
+                # Look for strings that start with "sk-" and are followed by 48-bit characters in each line
+                matches = re.findall(r'sk-\w{48}', line)
+                # Add the found matches to the list
+                matched_strings.extend(matches)
+
+        return matched_strings
+
     def gpt_rephraser(self, sentence):
-        api_keys = ['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', 
-                    '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',
-                    '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', 
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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'] 
+        
+        api_keys = self.get_api_key()
         proxy = {
             'http': 'http://localhost:7890',
             'https': 'http://localhost:7890'
         }
-        # 设置代理
+        # Set up a proxy
         openai.proxy = proxy  
-        # 定义提示
+        # Define the prompt
         prompt = f"Generate a random sentence piece with similar structures as: '{sentence}'"       
         for key in api_keys:
             try:
-                # 设置当前API key
+                # Set API key
                 openai.api_key = key              
-                # 调用API
+                # Use API key
                 response = openai.Completion.create(
                     engine='gpt-3.5-turbo-instruct',
                     prompt=prompt,
@@ -120,118 +58,35 @@ class Pic_LLMMixin(SimpleTrainer):
                     stop=None,
                     timeout=100
                 )               
-                # 如果成功，从响应中提取改写的句子并返回
+                # If successful, the rephrased sentence is extracted from the response and returned
                 rephrased_sentence = response.choices[0].text.strip()
                 return rephrased_sentence
             
             except Exception as e:
-                # 如果调用失败，打印错误信息并继续尝试下一个API key
+                # If the call fails, print the error message and continue trying the next API key
                 # print(f"Error with API key {key}: {e}")
                 continue
         
-        # 如果所有API keys都尝试失败，返回错误信息
+        # If all API keys fail to attempt, an error message is returned
         return "Failed to rephrase the sentence using all available API keys."
 
 
     def gpt_adjuster(self, sentence):
-        api_keys = ['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', 
-                    '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',
-                    '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', 
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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',
-                    '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'] 
+        api_keys = self.get_api_key()
         proxy = {
             'http': 'http://localhost:7890',
             'https': 'http://localhost:7890'
         }
-        # 设置代理
+        # Set up a proxy
         openai.proxy = proxy  
         # Define the prompt
         prompt = f"Rephrase and refine the following piece of sentence in more detail: '{sentence}'"
         # Generate text using the completions API
         for key in api_keys:
             try:
-                # 设置当前API key
+                # Set API key
                 openai.api_key = key              
-                # 调用API
+                # Use API key
                 response = openai.Completion.create(
                     engine='gpt-3.5-turbo-instruct',
                     prompt=prompt,
@@ -241,16 +96,16 @@ class Pic_LLMMixin(SimpleTrainer):
                     stop=None,
                     timeout=100
                 )               
-                # 如果成功，从响应中提取改写的句子并返回
+                # If successful, the rephrased sentence is extracted from the response and returned
                 adjusted_sentence = response.choices[0].text.strip()
                 return adjusted_sentence
             
             except Exception as e:
-                # 如果调用失败，打印错误信息并继续尝试下一个API key
+                # If the call fails, print the error message and continue trying the next API key
                 # print(f"Error with API key {key}: {e}")
                 continue
         
-        # 如果所有API keys都尝试失败，返回错误信息
+        # If all API keys fail to attempt, an error message is returned
         return "Failed to rephrase the sentence using all available API keys."
 
 
